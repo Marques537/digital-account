b7c3cb28e8322da156072c82ab77013a7b42ebd8
Matheus Marques <matheusm537@gmail.com>
2022-03-31 12:26:12 -0300

    fix synchronize enviroment

--- ormconfig.js
+++ ormconfig.js
@@ -2,5 +2,5 @@ module.exports = {
   type: process.env.DATABASE_TYPE,
   database: process.env.DATABASE_NAME,
   entities: ['dist/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: process.env.SYNCHRONIZE,
 };
