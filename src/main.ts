20ab20aa11c37b74ade9a6c07747dba2878032f3
Matheus Marques <matheusm537@gmail.com>
2022-03-31 12:38:46 -0300

    fix launch enviroments

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
 
   SwaggerModule.setup('swagger', app, document);
-  await app.listen(3001);
+  await app.listen(process.env.LAUNCH_PORT);
 }
 bootstrap();
